82c31a8b54712a07c41577cbb1a5feab0395dfeb
Vuk Perovic <vukperovic@Vuks-MacBook-Pro.local>
2024-05-18 22:12:47 -0700

    added tkinter to display it on a window

--- guessNumber.py
+++ guessNumber.py
@@ -1,33 +1,53 @@
 import random  
 import time                          
+from tkinter import *
+from tkinter import ttk
+from tkinter import simpledialog, messagebox
 
-guessesTaken = 0                     
 
-print("hello what is ur name")
-myName = input()
-number = random.randint(1, 2)
-print("hello " + myName + " i am thinking of a number between 1 anfd 2")
-time.sleep(1)
 
-for guessesTaken in range(6):
-    print("take a guess")
-    guess = int(input())
 
-    if guess < number:
-        print('too low, again')
 
-    if guess > number:
-        print('too high, go again')
+
+
+def play_game():
+    number = random.randint(1, 2)
+    guessesTaken = 0                     
+
+    myName = simpledialog.askstring("Guessing Game", "Hello and welcome! What is your name?")
+
+    messagebox.showinfo("hello, " + myName + "i am thinking of number between 1 and 2")
+    time.sleep(1)
+                                                                                   
+    while guessesTaken < 3:
+        guess = simpledialog.askinteger("take a guess!")
+        
+        guessesTaken += 1
+
+        if guess < number:
+            messagebox.showinfo('too low, again')
+
+        if guess > number:
+            messagebox.showifo('too high, go again')
+
+        else:
+            break                                                                   
 
     if guess == number:
-        break
+        if guessesTaken == 1:
+            messagebox.showinfo("u actually got it on the first try, good stuff")
+        else:
+            messagebox.showinfo("it only took you " + str(guessesTaken) + " times to get it, good job though")                
 
-if guess == number:
-    guessesTaken += 1
-    if guessesTaken == 1:
-        print("u actually got it on the first try, good stuff")
     else:
-        print("it only took you " + str(guessesTaken) + " times to get it, good job though")
+        messagebox.showinfo('nope, the number i was thinking of is ' + str(number))                                   
 
-if guess != number:
-    print('nope, the number i was thinking of is ' + str(number))
+
+root = Tk()
+frm = ttk.Frame(root, padding=100)
+frm.grid()
+
+
+play_game()
+
+root.mainloop()
